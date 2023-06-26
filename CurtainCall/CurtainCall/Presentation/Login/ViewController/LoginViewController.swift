83cb37a689bbe065a2ceec36ad17d77da1b05144
mandos1995 <mandos19950620@gmail.com>
2023-06-26 14:31:38 +0900

    🔧 chore: 폴더 구조 변경

--- CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
+++ CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  CurtainCall
 //
 //  Created by 김민석 on 2023/06/26.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
     }
-
-
 }
-
