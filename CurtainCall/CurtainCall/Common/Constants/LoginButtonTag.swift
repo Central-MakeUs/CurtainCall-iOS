6807e00f84d892c7a8a91e0439cdc17ce4e9485b
mandos1995 <mandos19950620@gmail.com>
2023-06-26 18:12:23 +0900

    ✨ feat: 소셜 로그인 버튼에 tag 값 설정 및 tag 값으로 분기처리하도록 구현

---
+++ CurtainCall/CurtainCall/Common/Constants/LoginButtonTag.swift
@@ -0,0 +1,15 @@
+//
+//  LoginButtonTag.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/06/26.
+//
+
+import Foundation
+
+enum LoginButtonTag {
+    static let kakaoTag = 1
+    static let naverTag = 2
+    static let facebookTag = 3
+    static let appleTag = 4
+}
--- CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
+++ CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
@@ -23,24 +23,28 @@ final class LoginViewController: UIViewController {
     private let kakaoLoginButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: ImageNamespace.loginButtonKakao), for: .normal)
+        button.tag = LoginButtonTag.kakaoTag
         return button
     }()
     
     private let naverLoginButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: ImageNamespace.loginButtonNaver), for: .normal)
+        button.tag = LoginButtonTag.naverTag
         return button
     }()
     
     private let facebookLoginButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: ImageNamespace.loginButtonFacebook), for: .normal)
+        button.tag = LoginButtonTag.facebookTag
         return button
     }()
     
     private let appleLoginButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: ImageNamespace.loginButtonApple), for: .normal)
+        button.tag = LoginButtonTag.appleTag
         return button
     }()
     
@@ -58,6 +62,7 @@ final class LoginViewController: UIViewController {
     private func configureUI() {
         configureSubviews()
         configureConstraints()
+        addTargets()
     }
     
     private func configureSubviews() {
@@ -71,6 +76,31 @@ final class LoginViewController: UIViewController {
     private func configureConstraints() {
         loginButtonStackView.snp.makeConstraints {
             $0.center.equalToSuperview()
+        }
+    }
+    
+    private func addTargets() {
+        [kakaoLoginButton, naverLoginButton, facebookLoginButton, appleLoginButton].forEach {
+            $0.addTarget(self, action: #selector(loginButtonTouchUpInside), for: .touchUpInside)
+        }
+    }
+    
+    // MARK: - Action
+    
+    @objc
+    private func loginButtonTouchUpInside(_ sender: UIButton) {
+        // TODO: 로그인 로직 처리
+        switch sender.tag {
+        case LoginButtonTag.kakaoTag:
+            print("kakaoLogin Tapped")
+        case LoginButtonTag.naverTag:
+            print("naverLogin Tapped")
+        case LoginButtonTag.facebookTag:
+            print("facebookLogin Tapped")
+        case LoginButtonTag.appleTag:
+            print("appleLogin Tapped")
+        default:
+            fatalError("Login Tag invalid")
         }
     }
 }
