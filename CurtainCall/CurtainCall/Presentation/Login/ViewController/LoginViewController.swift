bd62be6366175c94118354fbeab886e33f4bf4ed
mandos1995 <mandos19950620@gmail.com>
2023-06-26 17:53:38 +0900

    💄 UI: 소셜로그인 UI 추가

--- CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
+++ CurtainCall/CurtainCall/Presentation/Login/ViewController/LoginViewController.swift
@@ -7,10 +7,70 @@
 
 import UIKit
 
+import SnapKit
+
 final class LoginViewController: UIViewController {
+    
+    // MARK: - UI properties
+    
+    private let loginButtonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private let kakaoLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: ImageNamespace.loginButtonKakao), for: .normal)
+        return button
+    }()
+    
+    private let naverLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: ImageNamespace.loginButtonNaver), for: .normal)
+        return button
+    }()
+    
+    private let facebookLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: ImageNamespace.loginButtonFacebook), for: .normal)
+        return button
+    }()
+    
+    private let appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: ImageNamespace.loginButtonApple), for: .normal)
+        return button
+    }()
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    private func configureSubviews() {
+        view.backgroundColor = .white
+        view.addSubview(loginButtonStackView)
+        [kakaoLoginButton, naverLoginButton, facebookLoginButton, appleLoginButton].forEach {
+            loginButtonStackView.addArrangedSubview($0)
+        }
+    }
+    
+    private func configureConstraints() {
+        loginButtonStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
 }
