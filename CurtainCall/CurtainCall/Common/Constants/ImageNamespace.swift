4daa10671a58c65e8b09d6653496be950b92bedc
mandos1995 <mandos19950620@gmail.com>
2023-06-26 17:47:10 +0900

    ✨ feat: 이미지 네임스페이스 생성 및 크기에 안맞는 이미지 에셋 제거

---
+++ CurtainCall/CurtainCall/Common/Constants/ImageNamespace.swift
@@ -0,0 +1,15 @@
+//
+//  ImageNamespace.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/06/26.
+//
+
+import Foundation
+
+enum ImageNamespace {
+    static let loginButtonApple = "login_button_apple"
+    static let loginButtonNaver = "login_button_naver"
+    static let loginButtonKakao = "login_button_kakao"
+    static let loginButtonFacebook = "login_button_facebook"
+}
