d007d7721fba0e27110acbfb3c3a09626616107b
mandos1995 <mandos19950620@gmail.com>
2023-06-26 14:11:19 +0900

    🎉 커튼콜 프로젝트 생성

---
+++ CurtainCall/CurtainCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
