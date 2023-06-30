3c4815c5a6f6ff535852e42ac0376f0d1b4f0dc1
mandos1995 <mandos19950620@gmail.com>
2023-06-30 12:27:59 +0900

    🔧 chore: Facebook Login 관련 셋팅

--- CurtainCall/CurtainCall/App/AppDelegate.swift
+++ CurtainCall/CurtainCall/App/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import KakaoSDKCommon
+import FacebookCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         KakaoSDK.initSDK(appKey: Secret.KAKAO_NATIVE_APP_KEY)
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
     }
