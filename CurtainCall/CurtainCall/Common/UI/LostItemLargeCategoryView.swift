b82d245bb9fa662a62ee8dced06802579341f289
mandos1995 <mandos19950620@gmail.com>
2023-08-01 23:59:27 +0900

    🚧  UI: 분실물 UI 작업 진행중..

---
+++ CurtainCall/CurtainCall/Common/UI/LostItemLargeCategoryView.swift
@@ -0,0 +1,32 @@
+//
+//  LostItemLargeCategoryView.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/07/31.
+//
+
+import UIKit
+
+final class LostItemLargeCategoryView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        
+    }
+    
+    private func configureSubviews() {
+        
+    }
+    
+    private func configureConstarints() {
+        
+    }
+}
