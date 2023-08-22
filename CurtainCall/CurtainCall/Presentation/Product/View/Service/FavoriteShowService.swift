049c7cb600311ec0f8701932608d8db086a287aa
mandos1995 <mandos19950620@gmail.com>
2023-08-22 18:22:52 +0900

    feat: 즐겨찾기 공연 API service 작성

---
+++ CurtainCall/CurtainCall/Presentation/Product/View/Service/FavoriteShowService.swift
@@ -0,0 +1,64 @@
+//
+//  FavoriteShowService.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/08/22.
+//
+
+import Foundation
+
+import Moya
+import SwiftKeychainWrapper
+
+enum FavoriteShowAPI {
+    case putShow(id: String)
+    case deleteShow(id: String)
+    case getShowList(id: String)
+    case getMyFavoriteShow(memberId: String)
+    
+}
+
+extension FavoriteShowAPI: TargetType {
+    var baseURL: URL { URL(string: Secret.BASE_URL)! }
+    
+    var path: String {
+        switch self {
+        case .putShow(let id):
+            return "/shows/\(id)/favorite"
+        case .deleteShow(let id):
+            return "/shows/\(id)/favorite"
+        case .getShowList:
+            return "/member/favorite"
+        case .getMyFavoriteShow(let memerId):
+            return "/member/\(memerId)/favorite"
+        }
+    }
+    var method: Moya.Method {
+        switch self {
+        case .putShow: return .put
+        case .deleteShow: return .delete
+        case .getShowList: return .get
+        case .getMyFavoriteShow: return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .putShow:
+            return .requestPlain
+        case .deleteShow:
+            return .requestPlain
+        case .getShowList(let id):
+            var param: [String: Any] = [:]
+            param.updateValue(id, forKey: "showIds")
+            return .requestParameters(parameters: param, encoding: URLEncoding.default)
+        case .getMyFavoriteShow:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        let accessToken = KeychainWrapper.standard.string(forKey: .accessToken) ?? ""
+        return ["Authorization": "Bearer \(accessToken)"]
+    }
+}
