6f93e918a7442e99dc05aa4ba9d3d63f43303b0a
mandos1995 <mandos19950620@gmail.com>
2023-08-28 14:05:05 +0900

    feat: Swipe back 기능 안되는 버그 해결

---
+++ CurtainCall/CurtainCall/Common/Extension/UINavigationController + Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController + Extension.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/08/28.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+    
+}
