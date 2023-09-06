406d0911348c0a95d0ad3852eb5f57fe201a5bbd
mandos1995 <mandos19950620@gmail.com>
2023-09-06 18:13:09 +0900

    feat: ChatClient 싱글톤 객체 생성

---
+++ CurtainCall/CurtainCall/Common/GetStream/ChatClient.swift
@@ -0,0 +1,30 @@
+//
+//  ChatClient.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 2023/09/06.
+//
+
+import Foundation
+
+import StreamChat
+
+extension ChatClient {
+    static let shared = ChatClient(config: ChatClientConfig(apiKey: .init(Secret.CHAT_CLIENT_APPKEY)))
+    
+    
+//    func connectUser(userId: String, name: String) {
+//        let userInfo = UserInfo(id: userId, name: name)
+//        ChatClient.shared.connectUser(userInfo: userInfo, token: .development(userId: <#T##UserId#>))
+//    }
+    func connectSuperUser() {
+        ChatClient.shared.connectUser(
+            userInfo: .init(id: "super-user"),
+            token: .development(userId: "super-user")
+        )
+    }
+//    func createChannel(id: String) {
+//        let channelId = ChannelId(type: .messaging, id: id)
+//        ChatClient.shared.chan
+//    }
+}
